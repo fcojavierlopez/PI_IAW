cdf28b0eb4b793e9b237b72c68770b970992b338
fcojavierlopez <fcojavierlopezleon@gmail.com>
2017-05-20 10:38:45 +0200

    Add files via upload

---
+++ fonts/blanco.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href=" ">
+    </head>
+    <body>
+      <?php
+      include '../usuarios/conexion.php';
+$usuario=$_GET['usuario'];
+
+if ($result = $connection->query("UPDATE usuarios SET `TEMA` = '1' WHERE CORREO_ELECTRONICO ='$usuario';"))
+   {
+  header ("Location: ../usuarios/home.php");
+} else {
+
+      echo "Error: " . $result . "<br>" . mysqli_error($connection);
+}
+
+unset($connection);
+?>
+
+       ?>
+      <script type="text/javascript" src=" "></script>
+    </body>
+</html>
